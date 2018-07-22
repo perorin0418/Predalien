e30b8e7b5f001e7770dd17169ce2a5e0dcc01934
perorin <perorin@MyComputer>
2018-07-22 20:15:17 +0900

    ・抽象メソッドを使用するように変更

--- src/org/net/perorin/predalien/server/PredalienDatumReciever.java
+++ src/org/net/perorin/predalien/server/PredalienDatumReciever.java
@@ -1,72 +1,23 @@
 package org.net.perorin.predalien.server;
 
 import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Properties;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import org.net.perorin.predalien.client.PredalienDatum;
-import org.net.perorin.predalien.client.PredalienUtil;
+import org.net.perorin.predalien.common.PredalienAbstractReciever;
+import org.net.perorin.predalien.common.PredalienDatum;
+import org.net.perorin.predalien.common.PredalienUtil;
 
-public class PredalienDatumReciever {
+public class PredalienDatumReciever extends PredalienAbstractReciever {
 
-	/** tmpファイルパス */
-	private File tmpFilePath = new File(PredalienUtil.getTempFilePath() + File.separator + "Predalien");
+	@Override
+	public String getPattern() {
+		return "^[0-9]{16}-PredalienDatum.*$";
+	}
 
-	/** tmpファイル読み込み用 */
-	private Properties properties = new Properties();
-
-	/** ファイルソート用 */
-	private FileComparator fc = new FileComparator();
-
-	/** ファイルフィルターの正規表現 */
-	private String tmpPattern = "^[0-9]{16}-PredalienDatum.*$";
-
-	public void run() {
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
-
-			@Override
-			public void run() {
-
-				// tmpフォルダ内のファイル一覧取得
-				File[] files = tmpFilePath.listFiles(filter);
-
-				// 取得できなけりゃリターン
-				if (files == null) {
-					return;
-				}
-
-				Arrays.sort(files, fc);
-				for (File file : files) {
-					try {
-						// ファイル取得
-						FileInputStream fis = new FileInputStream(file);
-
-						// ファイル読み込み
-						properties.load(fis);
-
-						// ファイルロック解除
-						fis.close();
-
-						// データムに変換
-						PredalienDatum datum = load(properties);
-
-						// レシーブ呼び出し
-						recieve(datum);
-
-						// ファイルは削除
-						file.delete();
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}, 0, 100);
+	@Override
+	public boolean recieve(File file) {
+		PredalienDatum datum = PredalienUtil.convProperties2Datum(file);
+		recieve(datum);
+		return true;
 	}
 
 	/**
@@ -77,48 +28,4 @@ public class PredalienDatumReciever {
 		// NOP
 	}
 
-	/**
-	 * tmpファイル読み込み
-	 * @param properties
-	 * @return
-	 */
-	private PredalienDatum load(Properties properties) {
-		String target = properties.getProperty("Target");
-		String name = properties.getProperty("Name");
-		String className = properties.getProperty("ClassName");
-		String mouseInfo = properties.getProperty("MouseInfo");
-		String relaMousePos = properties.getProperty("RelaMousePos");
-		String absMousePos = properties.getProperty("AbsMousePos");
-		String keyInfo = properties.getProperty("KeyInfo");
-		String keyCode = properties.getProperty("KeyCode");
-		String keyModifiers = properties.getProperty("KeyModifiers");
-		String delay = properties.getProperty("Delay");
-		PredalienDatum datum = new PredalienDatum(target, name, className, delay);
-		datum.registMouse(mouseInfo, relaMousePos, absMousePos);
-		datum.registKey(keyInfo, keyCode, keyModifiers);
-		return datum;
-	}
-
-	FileFilter filter = new FileFilter() {
-
-		public boolean accept(File pathname) {
-			return pathname.getName().matches(tmpPattern);
-		}
-	};
-
-	class FileComparator implements Comparator<File> {
-
-		public int compare(File f1, File f2) {
-			long l1 = Long.parseLong(f1.getName().substring(0, 16).replaceAll("^0+", ""));
-			long l2 = Long.parseLong(f2.getName().substring(0, 16).replaceAll("^0+", ""));
-			if (l1 < l2) {
-				return -1;
-			} else if (l1 > l2) {
-				return 1;
-			} else {
-				return 0;
-			}
-		}
-
-	}
 }
