cecd019659e058d8242a79419ab330bf6377579c
豊島 大輝 <t-toyoshima@gyoseiq.co.jp>
2018-07-27 13:08:13 +0900

    ・PredalienDataに関するメソッド追加

--- src/org/net/perorin/predalien/common/PredalienUtil.java
+++ src/org/net/perorin/predalien/common/PredalienUtil.java
@@ -28,7 +28,9 @@ public class PredalienUtil {
 
 	/**
 	 * 文字列のマウス位置をポイントに変換するべ
-	 * @param mousePos マウス位置
+	 * 
+	 * @param mousePos
+	 *            マウス位置
 	 * @return マウス位置
 	 */
 	public static Point convMousePosString2Point(String mousePos) {
@@ -41,7 +43,9 @@ public class PredalienUtil {
 
 	/**
 	 * ポイントのマウス位置を文字列に変換するぞなもし
-	 * @param mousePos マウス位置
+	 * 
+	 * @param mousePos
+	 *            マウス位置
 	 * @return マウス位置
 	 */
 	public static String convMousePosPoint2String(Point mousePos) {
@@ -50,6 +54,7 @@ public class PredalienUtil {
 
 	/**
 	 * 一時ファイルの格納場所を返却するプリ
+	 * 
 	 * @return 一時ファイルの格納場所
 	 */
 	public static String getTempFilePath() {
@@ -77,7 +82,9 @@ public class PredalienUtil {
 
 	/**
 	 * 記録中かどうか設定
-	 * @param b 記録中
+	 * 
+	 * @param b
+	 *            記録中
 	 */
 	public static void setRecording(boolean b) {
 		createTempDir();
@@ -86,7 +93,8 @@ public class PredalienUtil {
 			file.delete();
 		}
 		try {
-			File file = File.createTempFile("Recording-", ".tmp",new File(getTempFilePath() + File.separator + "Predalien"));
+			File file = File.createTempFile("Recording-", ".tmp", new File(getTempFilePath() + File.separator
+					+ "Predalien"));
 			file.deleteOnExit();
 			PrintWriter pw = new PrintWriter(file);
 			pw.println(String.valueOf(b));
@@ -98,6 +106,7 @@ public class PredalienUtil {
 
 	/**
 	 * 記録中かどうか
+	 * 
 	 * @return 記録中かどうか
 	 */
 	public static boolean isRecording() {
@@ -115,6 +124,12 @@ public class PredalienUtil {
 	/** tmpファイル読み込み用 */
 	private static Properties properties = new Properties();
 
+	/**
+	 * propertiesファイルをデータムに変換
+	 * 
+	 * @param file
+	 * @return
+	 */
 	public static PredalienDatum convProperties2Datum(File file) {
 		try {
 			// ファイル取得
@@ -127,7 +142,7 @@ public class PredalienUtil {
 			fis.close();
 
 			// データムに変換
-			PredalienDatum datum = propertiesLoad(properties);
+			PredalienDatum datum = propertiesLoadDatum(properties);
 
 			return datum;
 
@@ -138,6 +153,72 @@ public class PredalienUtil {
 		return null;
 	}
 
+	/**
+	 * propertiesファイルをデータに変換
+	 * 
+	 * @param file
+	 * @return
+	 */
+	public static PredalienData convProperties2Data(File file) {
+		try {
+			// ファイル取得
+			FileInputStream fis = new FileInputStream(file);
+
+			// ファイル読み込み
+			properties.load(fis);
+
+			// ファイルロック解除
+			fis.close();
+
+			// データに変換
+			PredalienData datum = propertiesLoadData(properties);
+
+			return datum;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * fileにpropertiesの形式でdataを書き込む
+	 * 
+	 * @param file
+	 * @param data
+	 */
+	public static void writeDataOnFile(File file, PredalienData data) {
+		try {
+			PrintWriter fw = new PrintWriter(file);
+			int count = 100;
+			for (PredalienDatum datum : data) {
+				String prefix = "pd" + String.valueOf(count) + ".";
+				fw.println(prefix + "Target = " + datum.getTarget());
+				fw.println(prefix + "Name = " + datum.getName());
+				fw.println(prefix + "ClassName = " + datum.getClassName());
+				fw.println(prefix + "MouseInfo = " + datum.getMouseInfo());
+				fw.println(prefix + "RelaMousePos = " + datum.getRelaMousePos());
+				fw.println(prefix + "AbsMousePos = " + datum.getAbsMousePos());
+				fw.println(prefix + "KeyInfo = " + datum.getKeyInfo());
+				fw.println(prefix + "KeyCode = " + datum.getKeyCode());
+				fw.println(prefix + "KeyModifiers = " + datum.getKeyModifiers());
+				fw.println(prefix + "Delay = " + datum.getDelay());
+				fw.println();
+				count += 100;
+			}
+			fw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * fileにpropertisの形式でdatumを書き込む
+	 * 
+	 * @param file
+	 * @param datum
+	 */
 	public static void writeDatumOnFile(File file, PredalienDatum datum) {
 		try {
 			PrintWriter fw = new PrintWriter(file);
@@ -157,12 +238,38 @@ public class PredalienUtil {
 		}
 	}
 
+	private static PredalienData propertiesLoadData(Properties properties) {
+		PredalienData data = new PredalienData();
+
+		for (int count = 1; count <= 99999999; count++) {
+			String prefix = "pd" + String.valueOf(count) + ".";
+			if (properties.getProperty(prefix + "Target") != null) {
+				String target = properties.getProperty(prefix + "Target");
+				String name = properties.getProperty(prefix + "Name");
+				String className = properties.getProperty(prefix + "ClassName");
+				String mouseInfo = properties.getProperty(prefix + "MouseInfo");
+				String relaMousePos = properties.getProperty(prefix + "RelaMousePos");
+				String absMousePos = properties.getProperty(prefix + "AbsMousePos");
+				String keyInfo = properties.getProperty(prefix + "KeyInfo");
+				String keyCode = properties.getProperty(prefix + "KeyCode");
+				String keyModifiers = properties.getProperty(prefix + "KeyModifiers");
+				String delay = properties.getProperty(prefix + "Delay");
+				PredalienDatum datum = new PredalienDatum(target, name, className, delay);
+				datum.registMouse(mouseInfo, relaMousePos, absMousePos);
+				datum.registKey(keyInfo, keyCode, keyModifiers);
+				data.add(datum);
+			}
+		}
+		return data;
+	}
+
 	/**
 	 * tmpファイル読み込み
+	 * 
 	 * @param properties
 	 * @return
 	 */
-	private static PredalienDatum propertiesLoad(Properties properties) {
+	private static PredalienDatum propertiesLoadDatum(Properties properties) {
 		String target = properties.getProperty("Target");
 		String name = properties.getProperty("Name");
 		String className = properties.getProperty("ClassName");
@@ -179,7 +286,10 @@ public class PredalienUtil {
 		return datum;
 	}
 
-	public static void cleanTmp(){
+	/**
+	 * tmpフォルダ内の不要なファイルを削除する
+	 */
+	public static void cleanTmp() {
 		// TODO tmpファルダ内のお掃除
 	}
 
